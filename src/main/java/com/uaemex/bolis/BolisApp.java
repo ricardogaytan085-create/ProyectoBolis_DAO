dd94d2270caca68fba3a8dc85ce20a7860455323
WendyAr06 <gwendolynealcantarlopez@gmail.com>
2026-06-09 22:09:44 -0600

    Add comment for edit by Ricardo
    Added a comment indicating an edit by Ricardo.

--- src/main/java/com/uaemex/bolis/BolisApp.java
+++ src/main/java/com/uaemex/bolis/BolisApp.java
@@ -3,7 +3,7 @@ package com.uaemex.bolis;
 import com.uaemex.bolis.controller.AppController;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+//Edición Ricardo
 public class BolisApp extends Application {
     public void start(Stage stage) {
         new AppController().start(stage);
