9a0aa283c0c895df32579177ba0e1b7b96e5f2dc
WendyAr06 <gwendolynealcantarlopez@gmail.com>
2026-06-09 22:09:02 -0600

    Add comment for editing reference in Ui.java

--- src/main/java/com/uaemex/bolis/view/Ui.java
+++ src/main/java/com/uaemex/bolis/view/Ui.java
@@ -11,7 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
+//Edición Ricardo
 public final class Ui {
     private static final String STYLESHEET = "/styles.css";
 
