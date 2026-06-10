1be285511a5b574036b55aaaf7b738d1f1ad81b2
WendyAr06 <gwendolynealcantarlopez@gmail.com>
2026-06-09 18:31:21 -0600

    Add validation for Boli constructor parameters
    Edición Wendy

--- src/main/java/com/uaemex/bolis/model/Boli.java
+++ src/main/java/com/uaemex/bolis/model/Boli.java
@@ -1,5 +1,5 @@
 package com.uaemex.bolis.model;
-
+//Editado Gwendy
 public record Boli(int id, String sabor, double precio, int stock) {
     public Boli {
         if (id < 0 || sabor == null || sabor.isBlank() || precio <= 0 || stock < 0) {
