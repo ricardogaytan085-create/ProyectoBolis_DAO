9b9ce4fb630f90dca8b8a7b241d7495efb0b1dfb
WendyAr06 <gwendolynealcantarlopez@gmail.com>
2026-06-09 22:03:14 -0600

    Add comment in ModelSmokeTest.java
     edit by Ricardo.

--- src/test/java/com/uaemex/bolis/model/ModelSmokeTest.java
+++ src/test/java/com/uaemex/bolis/model/ModelSmokeTest.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//Edicion Ricardo
 public class ModelSmokeTest {
     @Test
     void initVentaYReportes() throws Exception {
