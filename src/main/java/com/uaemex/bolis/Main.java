eadbed260dd7842a807eec2a90ff4ec73f881f5a
WendyAr06 <gwendolynealcantarlopez@gmail.com>
2026-06-09 22:05:43 -0600

    Remove unnecessary comment in Main.java

--- src/main/java/com/uaemex/bolis/Main.java
+++ src/main/java/com/uaemex/bolis/Main.java
@@ -1,7 +1,7 @@
 package com.uaemex.bolis;
 
 import javafx.application.Application;
-
+//Edición Ricardo
 public class Main {
     public static void main(String[] args) {
         Application.launch(BolisApp.class, args);
